21da860c2397fccc90a31de8115e1e4278b93e6e
SE Gyges <segyges@gmail.com>
2025-09-18 16:43:08 -0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-AI Tribalism related utils
+# AI Tribalism related utils
+
+Goals:
+1) AI Blocklist subscriber blocklist
+2) AI voluntarily labeller
+3) AI general-purpose mod system (big stretch and likely not in this repo)
+
+Current goal is 1), overlaps heavily in approach with 2
+
+## Inverse blocklist success criteria
+
+Input is a list of bluesky blocklist URLs
+Result is that all subscribers to those blocklists get pushed to a blocklist on Bluesky
+
+For a stretch criteria, keep the option to tail the firehose rather than hammering constellation
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use tokio;
+
+struct ConstellationClient {
+	root_uri: String,
+}
+
+impl ConstellationClient {
+    // Basic method syntax - this is how you add methods to structs
+    fn get_root_uri(&self) -> &str {
+        &self.root_uri
+    }
+
+		async fn get_links(
+			&self, target: &str,
+			collection: &str,
+			path: &str
+		) -> Result<String, Box<dyn std::error::Error>> {
+			let params = [
+				("target", target),
+				("collection", collection),
+				("path", path), 
+			];
+
+			let mut url = reqwest::Url::parse_with_params(
+				&self.get_root_uri(),
+				&params,
+			)?;
+			url.set_path("/links");
+			let body: String = reqwest::get(url)
+				.await?
+				.text()
+				.await?;
+
+			Ok(body)
+		}
+}
+
+#[tokio::main]
+async fn main() {
+		let _list_of_anti_lists: &str = include_str!("./anti-ai-lists.txt");
+		let cli = ConstellationClient { root_uri: "https://constellation.microcosm.blue/".to_string() };
+
+    let res = cli.get_links(
+			"at://did:plc:ltqxsidueahnlsandk4u4nwl/app.bsky.graph.list/3kdsk7c6rp22w",
+			"app.bsky.graph.listblock",
+		".subject").await;
+    match res {
+        Ok(body) => println!("{}", body),
+        Err(e) => println!("Error: {}", e),
+    }
 }
